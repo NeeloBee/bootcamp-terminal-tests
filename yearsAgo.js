0300c53f5b19e077cbc29710ab2d707258a4e890
NeeloBee <boineelolk@gmail.com>
2021-09-16 14:05:38 +0200

    passing yearsAgo:forgot to save

---
+++ yearsAgo.js
@@ -0,0 +1,8 @@
+module.exports = function yearsAgo(jaar) {
+const year = new Date()
+  return year.getFullYear()-jaar;
+}
+
+//asserts:
+/* assert.equal((new Date().getFullYear() - 1976), yearsAgo(1976))
+assert.equal((new Date().getFullYear() - 2000), yearsAgo(2000));*/
